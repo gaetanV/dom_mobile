64dfcdea813794508edcee8ece510e928184c0c9
gaetanV <gaetan.vigneron@gmail.com>
2016-10-06 01:33:17 +0200

    move XY
    position absolute + height width !padding

--- src/lib/DOM.js
+++ src/lib/DOM.js
@@ -63,61 +63,7 @@ var DOM;
             scroll:[],
         }
 
-   
-        var EVENT=function(){
-            //@FRAME REFRESH 
-            var refresh = function(){ 
-                TOUCHEVENT.refresh();
-                DOMMOVE.refresh();
-            }
-            var click = function (e){     
-                TOUCHEVENT.click(e);   
-            }
-            
-            var resize=function(){
-                 var w={x:window.innerWidth,y:window.innerHeight};
-                //@OPTI RESIZE X
-                if(w.x!=mem.window.x){
-                    mem.window.x=w.x;
-                    for(var i in dom.resize.x){dom.resize.x[i]();}
-                }
-            }
-            
-            var mousemove=function(e){  
-               mem.mouse={x:e.clientX,y:e.clientY};
-               switch(dom.move){
-                   case "whereYouGo":
-                       TOUCHEVENT.mousemove(e);        
-                       break;
-                   case "dragAndDrop":
-                       DOMMOVE.mousemove(e);
-                       break;
-                   case "longclick": 
-                       break;
-               }
-            }
-            var mousewheel=function(e){
-                for(var i in dom.scroll){dom.scroll[i](e.detail>0);};
-            }
-            var mouseup=function(e){
-                dom.move=false;
-                //e.explicitOriginalTarget
-                if(dom.click){
-                    TOUCHEVENT.mouseup();
-                }
-                if(dom.dragAndDrop){DOMMOVE.stopDragAndDrop();}
-            }
-            
-            return{
-                click:click,
-                resize:resize,
-                mousemove:mousemove,
-                mousewheel:mousewheel,
-                mouseup:mouseup,
-                refresh:refresh
-            }
-        }();
-    
+
       
         var TOUCHEVENT=function(){
             var domEvents={
@@ -172,8 +118,9 @@ var DOM;
                var node=dom.click.target;
                dom.click.etat="longclick";
                if(node.ref && typeof node.ref.longclick==='function'){
-                   node.ref.longclick(dom.click); 
                    dom.move="longclick";
+                   node.ref.longclick(dom.click); 
+                   
                
                 }
             }
@@ -273,14 +220,18 @@ var DOM;
          var DOMMOVE=function(){
              var domEvents={
                  refresh:function(){
+                      
                     var dm=dom.dragAndDrop;
                     if(dm.refresh){
+                        
                             dm.dom.style.left= dm.posEnd.x;
                             dm.dom.style.top= dm.posEnd.y;
+                            
                             dm.refresh=false;
                     }
                  },
                  mousemove:function(e){
+                     
                         var d=dom.dragAndDrop;
                         if(!d.mouse){d.mouse={x:e.clientX,y:e.clientY}}
                         var x=d.posStart.x;
@@ -302,30 +253,52 @@ var DOM;
                              case "xy":
                                 x-=(d.mouse.x-e.clientX)*d.speed; 
                                 y-=(d.mouse.y-e.clientY)*d.speed;  
+                                
                                 break;
                         }   
-                          
+                           console.log("move");
                         d.refresh=true;
                         d.posEnd={y:y,x:x};
+                       
+                 },
+                 mouseup:function(e){
+                      DOMMOVE.stopDragAndDrop();
                  }
              }
              var startDragAndDrop=function(domNode,way,speed,callback){
                 dom.move="dragAndDrop";
+                
                 var position = TOOLS.getOffset(domNode);
                 domNode.style.transition="none";
                 dom.dragAndDrop&&(stopDragAndDrop());
+                
+                var computedStyle = window.getComputedStyle(domNode); 
+         
                 dom.dragAndDrop={
                     timeStart:getTime(),
                     timeEnd:getTime()+1,
                     dom:domNode,
                     speed:speed,
                     mouse:false,
+                    css:{
+                       position:computedStyle.position,
+                       zIndex:computedStyle.zIndex,
+                    },
                     posEnd:{x:position.left ,y:position.top  },
                     posStart:{x:position.left ,y:position.top  },
                     way:way,
                     callback:callback,
                     refresh:false,
                 };
+                if(computedStyle.position!=="absolute"||computedStyle.position!=="fixed"){
+                    dom.dragAndDrop.dom.style.width=position.inner.width+"px";
+                     dom.dragAndDrop.dom.style.top=position.top+"px";
+                     dom.dragAndDrop.dom.style.left=position.left+"px";
+                    dom.dragAndDrop.dom.style.height=position.inner.height+"px";
+                    dom.dragAndDrop.dom.style.position="absolute";
+                    dom.dragAndDrop.dom.style.zIndex="200";
+                }
+                
             }
        
             var stopDragAndDrop =function(){ 
@@ -336,13 +309,21 @@ var DOM;
                 var x= Math.abs(domObj.posStart.x-domObj.posEnd.x);
                 var y= Math.abs(domObj.posStart.y-domObj.posEnd.y);
                 domObj.vitesse={x:1/(time*1/x),y:1/(time*1/y)}; 
+            
+                
+                dom.dragAndDrop.dom.style.zIndex=domObj.css.zIndex;
+                dom.dragAndDrop.dom.style.position=domObj.css.position;
+             
                 domObj.callback(domObj);
                 dom.dragAndDrop=false;
+             
+               
             }
              
              return{
                  refresh:domEvents.refresh,
                  mousemove:domEvents.mousemove,
+                 mouseup:domEvents.mouseup,
                  startDragAndDrop:startDragAndDrop,
                  stopDragAndDrop:stopDragAndDrop,
                
@@ -394,25 +375,32 @@ var DOM;
       
             var getOffset =function (node) {          
                     var e=jQueryToNatif(node);
-                    e = e.getBoundingClientRect();
-                    var h = node.clientHeight;
-                    var computedStyle = window.getComputedStyle(node); 
+                    var bc = e.getBoundingClientRect();
+                     var computedStyle = window.getComputedStyle(node); 
+                     var paddingTop=parseInt(computedStyle.paddingTop, 10);
+                     var paddingBottom=parseInt(computedStyle.paddingBottom, 10);
+                    var paddingLeft=parseInt(computedStyle.paddingLeft, 10);
+                     var paddingRight=parseInt(computedStyle.paddingRight, 10);
+                    
+                    
+                    var h = e.clientHeight;
+                   
                     var marginTop=parseInt(computedStyle.marginTop, 10);
                     var borderTop=parseInt(computedStyle.borderTopWidth, 10)
                     h += marginTop+borderTop;
                     h += parseInt(computedStyle.borderBottomWidth, 10);
                     h += parseInt(computedStyle.marginBottom, 10);
-                    var w = node.clientWidth;
+                    var w = e.clientWidth;
                     w += parseInt(computedStyle.marginRight, 10);
                     w += parseInt(computedStyle.borderRightWidth, 10);
                     var marginLeft=parseInt(computedStyle.marginLeft, 10);;
                     var borderLeft=parseInt(computedStyle.borderLeftWidth, 10);
                     w += borderLeft + marginLeft;
                     return {
-                        left: e.left + window.scrollX-marginLeft-borderLeft,
-                        top: e.top + window.scrollY-marginTop-borderTop,
+                        left: bc.left + window.scrollX-marginLeft-borderLeft,
+                        top: bc.top + window.scrollY-marginTop-borderTop,
                         outer:{height:h,width:w},
-                        inner:{height:node.clientHeight,width:node.clientWidth}
+                        inner:{height:e.clientHeight-(paddingTop+paddingBottom),width:e.clientWidth-(paddingRight+paddingLeft)}
                     }
              }
              return{
@@ -422,6 +410,66 @@ var DOM;
                   isInPage:isInPage,
              }
          }();   
+  
+     
+        var EVENT=function(){
+            //@FRAME REFRESH 
+            var refresh = function(){ 
+                TOUCHEVENT.refresh();
+                DOMMOVE.refresh();
+            }
+            var click = function (e){     
+                TOUCHEVENT.click(e);   
+            }
+            
+            var resize=function(){
+                 var w={x:window.innerWidth,y:window.innerHeight};
+                //@OPTI RESIZE X
+                if(w.x!=mem.window.x){
+                    mem.window.x=w.x;
+                    for(var i in dom.resize.x){dom.resize.x[i]();}
+                }
+            }
+            
+            var mousemove=function(e){  
+               mem.mouse={x:e.clientX,y:e.clientY};
+              
+               switch(dom.move){
+                   case "whereYouGo":
+                       TOUCHEVENT.mousemove(e);        
+                       break;
+                   case "dragAndDrop":
+                       DOMMOVE.mousemove(e);
+                       break;
+                   case "longclick": 
+                       break;
+               }
+            }
+            var mousewheel=function(e){
+                for(var i in dom.scroll){dom.scroll[i](e.detail>0);};
+            }
+            var mouseup=function(e){
+               
+                if(dom.click){
+                    TOUCHEVENT.mouseup();
+                }
+                if(dom.dragAndDrop){
+                    DOMMOVE.mouseup();
+                }
+                 dom.move=false;
+            }
+            
+            return{
+                click:click,
+                resize:resize,
+                mousemove:mousemove,
+                mousewheel:mousewheel,
+                mouseup:mouseup,
+                refresh:refresh
+            }
+        }();
+    
+  
   
         setInterval(EVENT.refresh,param.fps);
         //@DOM EVENT 
