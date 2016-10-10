bffef09060a1f18d62ee0a2e6deb537e59949bde
gaetanV <gaetan.vigneron@gmail.com>
2016-10-11 00:58:12 +0200

    Grunt

---
+++ src/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+        dest:{
+              src: ['lib/*.js'],
+             dest: "app/lib/DOM.js",
+        }
+    },
+    uglify: {
+        dest:{
+             src: ['lib/*.js'],
+             dest: "app/lib/DOM.js",
+        }
+    },
+    watch: {
+      files: ['lib/*.js'],
+      tasks: ['uglify']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['watch']);
+
+};
